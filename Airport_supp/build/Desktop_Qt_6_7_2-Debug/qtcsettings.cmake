52fe65fd73041cca022f9c369ffdaeb6caf9b08a
LeszczynskiK <leszczynskikrzysiu@gmail.com>
2025-03-27 09:15:38 +0100

    empty qml project created using qt quick

---
+++ Airport_supp/build/Desktop_Qt_6_7_2-Debug/qtcsettings.cmake
@@ -0,0 +1,10 @@
+# This file is managed by Qt Creator, do not edit!
+
+set("QT_QMAKE_EXECUTABLE" "/home/krzysiek89/Qt/6.7.2/gcc_64/bin/qmake" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
+set("CMAKE_C_COMPILER" "/usr/bin/x86_64-linux-gnu-gcc" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_FLAGS_INIT" "-DQT_QML_DEBUG" CACHE "STRING" "" FORCE)
+set("CMAKE_PROJECT_INCLUDE_BEFORE" "/home/krzysiek89/Desktop/QT_aplikacje/Plane_support_app_airport_visualisation/Airport_supp/build/Desktop_Qt_6_7_2-Debug/.qtc/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_COMPILER" "/usr/bin/g++" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_PREFIX_PATH" "/home/krzysiek89/Qt/6.7.2/gcc_64" CACHE "PATH" "" FORCE)
+set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
